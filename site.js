fd20ef8be0268a46d7bc9afeccc9ed8d7054d4ab
Karl Stolley <karl.stolley@gmail.com>
2017-09-06 18:14:52 -0500

    Append user data to #results

--- site.js
+++ site.js
@@ -1,14 +1,5 @@
-console.log("You have JavaScript!");
-function howManyPets(user) {
-  var plural = "s";
-  if (user.pets.length === 1) {
-    plural = "";
-  }
-  return user.name + " has " + user.pets.length + " pet" + plural;
-}
-var user = {
-  name: "Karl",
-  age: 39,
-  pets: ["Hank","Stewie"]
-  // TODO: programmatically access pets.length
-}
+$('#user-form').on('submit', function(e) {
+  var username = $('#username').val();
+  $('#results').append('<li><a href="https://api.github.com/users/' + username + '">' + username + '</a></li>');
+  e.preventDefault();
+});
