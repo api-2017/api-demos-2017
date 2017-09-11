17ee5427e11fd41cd0d22bc5831e3b69f23b25f6
Karl Stolley <karl.stolley@gmail.com>
2017-09-11 18:09:26 -0500

    Add dummy API data and append image to form

--- site.js
+++ site.js
@@ -1,3 +1,11 @@
+var user = {
+  "login": "profstolley",
+  "id": 10054112,
+  "avatar_url": "https://avatars3.githubusercontent.com/u/10054112?v=4"
+}
+
+$('#user-form').append('<img src="' + user['avatar_url'] + '" />');
+
 $('#user-form').on('submit', function(e) {
   var username = $('#username').val();
   $('#results').append('<li><a href="https://api.github.com/users/' + username + '">' + username + '</a></li>');
